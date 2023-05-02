eb6c005cb83a650416b35d1b49b66f7dae243672
maksat-01 <maksatarykbaiuulu@gmail.com>
2023-05-02 18:15:13 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
